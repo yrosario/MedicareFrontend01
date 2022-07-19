53c03fd3b6cf347ea8574b28cba98571e5a0c5d5
Yussel Rosario <yusselrosario@gmail.com>
2022-07-18 23:20:23 -0400

    feature. Added navigation bar and footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,18 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
 import { LogoffComponent } from './component/logoff/logoff/logoff.component';
 import { RegisterComponent } from './component/register/register.component';
+import { NavbarComponent } from './component/navigation/navbar/navbar.component';
+import { FooterComponent } from './component/navigation/footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     LogoffComponent,
-    RegisterComponent
+    RegisterComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
