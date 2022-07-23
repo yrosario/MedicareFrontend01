25731cb09d88c28fb3c03bd96db33c4979d1b968
Yussel Rosario <yusselrosario@gmail.com>
2022-07-23 11:49:18 -0400

    Added CRUD operations to product service

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -14,23 +14,34 @@ export class ProductService {
   constructor(private httpClient:HttpClient) { 
    }
 
+   /*Retrieves all products from http://localhost:8080/api/v1/product */
   getProducts(){
     return this.httpClient.get<[ProductEntity]>(API_URL + PRODUCT);
   }
 
+  /*Retrieves a specific product base on product id from http://localhost:8080/api/v1/product/:id */
   getProductById(id:number){
     return this.httpClient.get<ProductEntity>(API_URL+PRODUCT+"/" +id);
   }
 
-   deleteProductById(id:number){
-    for(let i = 0; i < this.products.length; i++){
-      if(id === this.products[i].pid){
-        this.products.splice(i,1);
-        console.log(this.products);
-        break;
-      }
-    }
-   }
+  /* Deletes a specific product base on id from http://localhost:8080/api/v1/product/:id */
+  deleteProductById(id:number){
+    this.httpClient.delete(API_URL+PRODUCT+"/"+id);
+  }
+
+  saveProduct(product:ProductEntity){
+    this.httpClient.post(`${API_URL}${PRODUCT}`,{observe: 'response', responseType: 'json'});
+  }
+
+  //  deleteProductById(id:number){
+  //   for(let i = 0; i < this.products.length; i++){
+  //     if(id === this.products[i].pid){
+  //       this.products.splice(i,1);
+  //       console.log(this.products);
+  //       break;
+  //     }
+  //   }
+  //  }
 
    findProductById(id){
     for(let product of this.products){
